337fe0b924a810cfcc2acab7c126fe623e5f5302
Henrique-Schultz <valente.schultz@gmail.com>
2026-05-09 18:57:52 -0300

    added CNAME to /public | added dev icon and rearranged layout

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Welcome } from './components/welcome/welcome';
 import { Description } from './components/description/description';
+import { Icon } from "./components/icon/icon";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Welcome, Description],
+  imports: [RouterOutlet, Welcome, Description, Icon],
   templateUrl: './app.html',
 })
 export class App {
